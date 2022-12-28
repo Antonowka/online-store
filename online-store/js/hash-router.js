dc38bf7c469b37715a0b0f455819bd44863e89a0
max7394 <mlysenko92@gmail.com>
2022-12-28 17:16:29 +0300

    feat: add routing to project

---
+++ online-store/js/hash-router.js
@@ -0,0 +1,70 @@
+// <<<---- СЮДА ИМПОРТИМ НАШИ ФАЙЛЫ СКРИПТЫ
+import createProducts from './main.js'
+import fillDetailPage from './detail.js'
+// ---->>>
+
+import myJson from './products.json' assert {
+  type: 'json'
+};
+
+const products = myJson.products;
+
+const pageTitle = "Online Store";
+
+const items = {}
+
+for (let i = 0; i < products.length; i++) {
+	var key = 'item-' + products[i].id;
+	var value = {
+		template: "/templates/temp-page-detail.html",
+		title: "Item | " + pageTitle,
+	};
+
+	items[key] = value;
+}
+
+const routes = {
+	404: {
+		template: "/templates/temp-page-404.html",
+		title: "404 | " + pageTitle,
+	},
+	"/": {
+		template: "/templates/temp-index.html",
+		title: "Home | " + pageTitle,
+	},
+	cart: {
+		template: "/templates/temp-page-cart.html",
+		title: "Cart | " + pageTitle,
+	},
+};
+
+Object.assign(routes, items)
+
+
+const locationHandler = async () => {
+	var location = window.location.hash.replace("#", "");
+
+	if (location.length == 0) {
+		location = "/";
+	}
+
+	const route = routes[location] || routes["404"];
+
+	const html = await fetch(route.template).then((response) => response.text());
+
+	document.querySelector("main").innerHTML = html;
+	// <<<---- СЮДА ВСТАВЛЯТЬ НАШИ ФУНКЦИИ ДЛЯ СТРАНИЦ
+	if (location === "/") {
+		createProducts();
+	}
+
+	if (location.startsWith("item")) {
+		fillDetailPage();
+	}
+	// ---->>>
+	document.title = route.title;
+};
+
+window.addEventListener("hashchange", locationHandler);
+
+locationHandler();
