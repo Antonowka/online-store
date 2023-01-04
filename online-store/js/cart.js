010f7ea70abcb4a18870b3b5cc036405721cbf7c
Anton <102463034+Antonowka@users.noreply.github.com>
2023-01-04 18:43:51 +0100

    fix: fixed git conflicts

--- online-store/js/cart.js
+++ online-store/js/cart.js
@@ -3,33 +3,27 @@ import myJson from './products.json' assert {
 };
 
 function fillCartPage() {
-  let cartProducts = JSON.parse(localStorage.getItem("RS-cart"));
+  const cartProducts = JSON.parse(localStorage.getItem("RS-cart"));
 
-  updateCart()
-  function updateCart() {
-    cartProducts = JSON.parse(localStorage.getItem("RS-cart"));
-    document.querySelector('.cart__prouducts__list__wrapper').innerHTML = '';
-    for (let i = 0; i < cartProducts.length; i++) {
-      let productCount = cartProducts[i].count;
-
-      let newItem = document.createElement('div');
-      newItem.className = 'cart__prouducts__list';
-      document.querySelector('.cart__prouducts__list__wrapper').append(newItem);
+  for (let i = 0; i < cartProducts.length; i++) {
+    let newItem = document.createElement('div');
+    newItem.className = 'cart__prouducts__list';
+    document.querySelector('.cart__prouducts__list__wrapper').append(newItem);
 
     // let itemN = document.createElement('div');
     // itemN.className = 'cart__prouducts__list__n';
     // itemN.innerHTML = `${i+1}`;
     // document.querySelectorAll('.cart__prouducts__list')[i].append(itemN);
 
-      let itemThumb = document.createElement('div');
-      itemThumb.className = 'cart__prouducts__list__photo';
-      const itemImg = new Image();
-      let productId = cartProducts[i].id
-      itemImg.src = `${myJson[productId-1].thumbnail}`;
-      itemImg.alt = "item img";
-      itemImg.className = "img__item";
-      itemThumb.appendChild(itemImg)
-      document.querySelectorAll('.cart__prouducts__list')[i].append(itemThumb);
+    let itemThumb = document.createElement('div');
+    itemThumb.className = 'cart__prouducts__list__photo';
+    const itemImg = new Image();
+    let productId = cartProducts[i].id
+    itemImg.src = `${myJson[productId-1].thumbnail}`;
+    itemImg.alt = "item img";
+    itemImg.className = "img__item";
+    itemThumb.appendChild(itemImg)
+    document.querySelectorAll('.cart__prouducts__list')[i].append(itemThumb);
 
     let itemNameWrapper = document.createElement('div');
     itemNameWrapper.className = 'cart__prouducts__info__wrapper';
@@ -59,27 +53,24 @@ function fillCartPage() {
     itemCountWrap.className = 'cart__prouducts__list__count';
     document.querySelectorAll('.cart__prouducts__list')[i].append(itemCountWrap);
 
-      let itemCountBtn = document.createElement('div');
-      itemCountBtn.className = 'cart__prouducts__list__count__items';
-      document.querySelectorAll('.cart__prouducts__list')[i].append(itemCountBtn);
+    let itemCountBtn = document.createElement('div');
+    itemCountBtn.className = 'cart__prouducts__list__count__items';
+    document.querySelectorAll('.cart__prouducts__list')[i].append(itemCountBtn);
 
-      let newBtnLeft = document.createElement('button');
-      newBtnLeft.className = 'btn_items_left';
-      newBtnLeft.id = `btn_items_left-${productId}`;
-      newBtnLeft.innerText = '-';
-      document.querySelectorAll('.cart__prouducts__list__count__items')[i].appendChild(newBtnLeft);
+    let newBtnMinus = document.createElement('button');
+    newBtnMinus.className = 'btn_items_left';
+    newBtnMinus.innerText = '-';
+    document.querySelectorAll('.cart__prouducts__list__count__items')[i].appendChild(newBtnMinus);
 
-      let itemCountSum = document.createElement('div');
-      itemCountSum.className = 'cart__prouducts__list__count__items__sum';
-      itemCountSum.id = `items__sum-${productId}`;
-      itemCountSum.innerHTML = productCount;
-      document.querySelectorAll('.cart__prouducts__list__count__items')[i].append(itemCountSum);
+    let itemCountSum = document.createElement('div');
+    itemCountSum.className = 'cart__prouducts__list__count__items__sum';
+    itemCountSum.innerHTML = "1";
+    document.querySelectorAll('.cart__prouducts__list__count__items')[i].append(itemCountSum);
 
-      let newBtnRight = document.createElement('button');
-      newBtnRight.className = 'btn_items_right';
-      newBtnRight.id = `btn_items_right-${productId}`;
-      newBtnRight.innerText = '+';
-      document.querySelectorAll('.cart__prouducts__list__count__items')[i].appendChild(newBtnRight);
+    let newBtnPlus = document.createElement('button');
+    newBtnPlus.className = 'btn_items_right';
+    newBtnPlus.innerText = '+';
+    document.querySelectorAll('.cart__prouducts__list__count__items')[i].appendChild(newBtnPlus);
 
     let itemPriceSum = document.createElement('div');
     itemPriceSum.className = 'cart__prouducts__list__count__sum';
@@ -91,8 +82,6 @@ function fillCartPage() {
     itemPriceSumSubtotal.innerHTML = `${myJson[productId-1].price} $`;
     document.querySelectorAll('.cart__prouducts__list')[i].append(itemPriceSumSubtotal);
   }
-
-  updateCount()
 }
 
 export default fillCartPage
