cd4d9aaf75adcb4bc531c758612535a01ed3c675
max7394 <mlysenko92@gmail.com>
2023-01-06 17:47:26 +0300

    feat: add interactive to purschase-modal

--- online-store/js/cart.js
+++ online-store/js/cart.js
@@ -20,7 +20,7 @@ function fillCartPage() {
       itemN.className = 'cart__prouducts__list__n';
       itemN.innerHTML = `${i+1}`;
       document.querySelectorAll('.cart__prouducts__list')[i].append(itemN);
-      
+
       let itemThumb = document.createElement('div');
       itemThumb.className = 'cart__prouducts__list__photo';
       const itemImg = new Image();
@@ -30,22 +30,22 @@ function fillCartPage() {
       itemImg.className = "img__item";
       itemThumb.appendChild(itemImg)
       document.querySelectorAll('.cart__prouducts__list')[i].append(itemThumb);
-      
-     
+
+
       let itemNameWrapper = document.createElement('div');
       itemNameWrapper.className = 'cart__prouducts__info__wrapper';
       document.querySelectorAll('.cart__prouducts__list__photo')[i].append(itemNameWrapper);
-      
+
       let itemName = document.createElement('div');
       itemName.className = 'cart__prouducts__list__detail__title';
       itemName.innerHTML = `${myJson[productId-1].name}`;
       document.querySelectorAll('.cart__prouducts__info__wrapper')[i].append(itemName);
-      
+
       let itemStock = document.createElement('div');
       itemStock.className = 'cart__prouducts__list__count__stock';
       itemStock.innerHTML = `Stock: ${myJson[productId-1].stock}`;
       document.querySelectorAll('.cart__prouducts__info__wrapper')[i].append(itemStock);
-     
+
       let itemRate = document.createElement('div');
       itemRate.className = 'cart__prouducts__list__detail__rating';
       itemRate.innerHTML = `Rating: ${myJson[productId-1].rating}`;
@@ -70,18 +70,18 @@ function fillCartPage() {
       itemCountSum.id = `items__sum-${productId}`;
       itemCountSum.innerHTML = productCount;
       document.querySelectorAll('.cart__prouducts__list__count__items')[i].append(itemCountSum);
-      
+
       let newBtnRight = document.createElement('button');
       newBtnRight.className = 'btn_items_right';
       newBtnRight.id = `btn_items_right-${productId}`;
       newBtnRight.innerText = '+';
       document.querySelectorAll('.cart__prouducts__list__count__items')[i].appendChild(newBtnRight);
-      
+
       let itemPriceSum = document.createElement('div');
       itemPriceSum.className = 'cart__prouducts__list__count__sum';
       itemPriceSum.innerHTML = `${myJson[productId-1].price * productCount}$`;
       document.querySelectorAll('.cart__prouducts__list')[i].append(itemPriceSum);
-      
+
       let itemPriceSumSubtotal = document.createElement('div');
       itemPriceSumSubtotal.className = 'cart__prouducts__list__count__subtotal';
       itemPriceSumSubtotal.innerHTML = `${myJson[productId-1].price} $`;
@@ -166,7 +166,7 @@ function fillCartPage() {
       sumCount += cartProducts[i].count;
       sumPrice += cartProducts[i].count * cartProducts[i].price;
     }
-    console.log(sumPrice);
+
     document.querySelector('.cart__summary__list__count__items').innerHTML = sumCount;
     document.querySelector('.cart__summary__list__total__sum').innerHTML = `${sumPrice}$`
 
@@ -174,6 +174,114 @@ function fillCartPage() {
     document.querySelector('.header__cost').innerHTML = `Cart total: ${sumPrice}$`
   }
   updateCount()
+
+  const btn_buy_now = document.querySelector('.btn__buynow');
+  const modalWindow = document.querySelector('.purchase__modal');
+  const transparentModal = document.querySelector('.transparent__modal');
+  const btn_modal_confirm = document.querySelector('.btn__modal__confirm');
+  const modal_input_name = document.getElementById('modal__input__name');
+  const modal_input_phone = document.getElementById('modal__input__phone');
+  const modal_input_adress = document.getElementById('modal__input__adress');
+  const modal_input_email = document.getElementById('modal__input__email');
+  const modal_input_cardNum = document.getElementById('modal__input__cardNum');
+  const modal_input_cardDate = document.getElementById('modal__input__card__date');
+  const modal_input_cvv = document.getElementById('modal__input__card__CVV');
+
+  btn_buy_now.addEventListener('click', showModalWindow);
+  transparentModal.addEventListener('click', closeModalWindow);
+  btn_modal_confirm.addEventListener('click', validateForms);
+  modal_input_cardDate.addEventListener('input', validateDate);
+  modal_input_cardNum.addEventListener('input', validateCardNum);
+  modal_input_cvv.addEventListener('input', validateCVV);
+
+  function showModalWindow() {
+    transparentModal.style.display = 'block';
+    modalWindow.style.display = 'block';
+  }
+
+  function closeModalWindow() {
+    transparentModal.style.display = 'none';
+    modalWindow.style.display = 'none';
+  }
+
+  function validateForms() {
+    if (!modal_input_name.checkValidity() || modal_input_name.value == '') {
+      document.querySelector('.error__name').innerHTML = 'Error. The field must contain at least two words, each at least 3 characters long';
+    } else {
+      document.querySelector('.error__name').innerHTML = '';
+    }
+
+    if (!modal_input_phone.checkValidity() || modal_input_phone.value == '') {
+      document.querySelector('.error__phone').innerHTML = 'Error. The field must start with '+', contain only numbers and be at least 9 characters long';
+    } else {
+      document.querySelector('.error__phone').innerHTML = '';
+    }
+
+    if (!modal_input_adress.checkValidity() || modal_input_adress.value == '') {
+      document.querySelector('.error__adress').innerHTML = 'Error. The field must contain at least three words, each at least 5 characters long';
+    } else {
+      document.querySelector('.error__adress').innerHTML = '';
+    }
+
+    if (!modal_input_email.checkValidity() || modal_input_email.value == '') {
+      document.querySelector('.error__email').innerHTML = 'Error. The field must contain email';
+    } else {
+      document.querySelector('.error__email').innerHTML = '';
+    }
+
+    if (!modal_input_cardNum.checkValidity() || modal_input_cardNum.value == '') {
+      document.querySelector('.error__cardNum').innerHTML = 'Error. The number of digits entered must be 16';
+    } else {
+      document.querySelector('.error__cardNum').innerHTML = '';
+    }
+
+    if (!modal_input_cardDate.checkValidity() || modal_input_cardDate.value == '') {
+      document.querySelector('.error__date').innerHTML = 'Error. Invalid date';
+    } else {
+      document.querySelector('.error__date').innerHTML = '';
+    }
+
+    if (!modal_input_cvv.checkValidity() || modal_input_cvv.value == '') {
+      document.querySelector('.error__cvv').innerHTML = 'Error. Invalid CVV';
+    } else {
+      document.querySelector('.error__cvv').innerHTML = '';
+    }
+
+    let modal_error = document.querySelectorAll('.modal__error');
+    let join_error_text = '';
+
+    for (let i = 0; i < modal_error.length; i++) {
+      join_error_text += modal_error[i].innerHTML
+    }
+
+    if (join_error_text == '') {
+      // console.log('ВСЕ ПОЛЯ ЗАПОЛНЕНЫ ВЕРНО!!!')
+      modalWindow.innerHTML = 'Thank you for your purchase. After 3 seconds you will be redirected to the main page'
+      setTimeout(successPurchase, 3000);
+    }
+
+  }
+
+  function validateDate(e) {
+    var x = e.target.value.replace(/\D/g, '').match(/(\d{0,2})(\d{0,2})/);
+    e.target.value = !x[2] ? x[1] : x[1] + '/' + x[2];
+  }
+
+  function validateCardNum(e) {
+    var y = e.target.value.replace(/\D/g, '').match(/(\d{0,16})/);
+    e.target.value = !y[2] ? y[1] : y[1];
+  }
+
+  function validateCVV(e) {
+    var z = e.target.value.replace(/\D/g, '').match(/(\d{0,3})/);
+    e.target.value = !z[2] ? z[1] : z[1];
+  }
+
+  function successPurchase() {
+    localStorage.setItem('RS-cart', JSON.stringify([]));
+    window.location.href = '';
+  }
+
 }
 
 export default fillCartPage
