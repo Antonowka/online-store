b579a720c655b21105c76a1051f8fce3e5ccd517
Anton <102463034+Antonowka@users.noreply.github.com>
2023-01-09 02:58:11 +0100

    feat: added sorting by price/rating/name

--- online-store/js/main.js
+++ online-store/js/main.js
@@ -2,71 +2,84 @@
 import updateHeader from './updateCart.js'
 import viewmode from './view-mode.js'
 
+const string = (a, b, dir = 1) => a?.localeCompare(b) * dir
+const number = (a, b, dir = 1) => (a - b) * dir
+
+const sortFnByField = {
+  default: () => 0,
+  price: number,
+  rating: number,
+  name: string,
+}
+
 function createProducts() {
   const userCardTemplate = document.querySelector("[data-user-template]")
   const userCardContainer = document.querySelector("[data-user-cards-container]")
   const searchInput = document.querySelector("[data-search]")
-
-  let products = []
-
-  searchInput.addEventListener("input", e => {
-    const value = e.target.value.toLowerCase()
-    products.forEach(product => {
-      const isVisible = product.name.toLowerCase().includes(value)
-      product.element.classList.toggle("hide", !isVisible)
-    })
-  })
-
+  const sorting = document.querySelector('.sort-menu')
+  const productContainer = document.querySelector('.products-container')
   const checkboxes = document.querySelectorAll('.filter-input')
 
+  function renderProducts(products) {
+    console.log('renderProducts', products)
+    productContainer.innerHTML = ''
+    // render product list as HTML here
+    products.forEach(product => {
+      const card = userCardTemplate.content.cloneNode(true).children[0]
+      const info = card.querySelector("[data-name]")
+      const cost = card.querySelector("[data-cost]")
+      const src = card.querySelector("[data-src]")
+      const href = card.querySelector("[data-href]")
+      const btn_add = card.querySelector("[data-btn-add]")
+      const rating = card.querySelector("[data-rating]")
+      const stock = card.querySelector("[data-stock]")
+      // const btn_buy_now = card.querySelector("[data-btn-buy-now]")
+      info.textContent = product.name
+      cost.innerHTML = product.price + ' $';
+      src.src = product.thumbnail;
+      href.href = `#item-${product.id}`;
+      rating.innerHTML = product.rating;
+      stock.innerHTML = `Stock: ${product.stock}`;
+      userCardContainer.append(card)
+      btn_add.id = `btn-add-${product.id}`;
+      // btn_buy_now.id = `btn-buy-now-${product.id}`;
+
+      btn_add.addEventListener('click', () => {
+        btn_add.classList.toggle('button-active')
+      })
+
+      for (let i = 0; i < checkboxes.length; i++) {
+        const checkbox = checkboxes[i];
+        checkbox.addEventListener('click', sortCheckbox)
+
+        function sortCheckbox() {
+          if (checkbox.value === product.category || checkbox.value === product.brand) {
+            info.textContent = product.name
+            cost.innerHTML = product.price + ' $';
+            src.src = product.thumbnail
+          } else if (!checkbox.checked) {
+            card.style.display = 'block';
+          } else {
+            card.style.display = 'none';
+          }
+        }
+      }
+      return {
+        name: product.name,
+        element: card
+      }
+    })
+  }
+
+  
   fetch('../js/products.json')
     .then(res => res.json())
     .then(data => {
-      products = data.map(product => {
-        const card = userCardTemplate.content.cloneNode(true).children[0]
-        const info = card.querySelector("[data-name]")
-        const cost = card.querySelector("[data-cost]")
-        const src = card.querySelector("[data-src]")
-        const href = card.querySelector("[data-href]")
-        const rating = card.querySelector("[data-rating]")
-        const stock = card.querySelector("[data-stock]")
-        const btn_add = card.querySelector("[data-btn-add]")
-        info.textContent = product.name
-        cost.innerHTML = product.price + ' $';
-        src.src = product.thumbnail;
-        rating.innerHTML = product.rating;
-        stock.innerHTML = `Stock: ${product.stock}`;
-        href.href = `#item-${product.id}`;
-        userCardContainer.append(card)
-        btn_add.id = `btn-add-${product.id}`;
-        btn_add.addEventListener('click', () => {
-          btn_add.classList.toggle('button-active')
-        })
+      // Use renderProducts() functions to render the products here
+      const productsRaw = data
+      renderProducts(productsRaw)
 
-        for (let i = 0; i < checkboxes.length; i++) {
-          const checkbox = checkboxes[i];
-          checkbox.addEventListener('click', sortCheckbox)
-
-          function sortCheckbox() {
-            if (checkbox.value === product.category || checkbox.value === product.brand) {
-              info.textContent = product.name
-              cost.innerHTML = product.price + ' $';
-              src.src = product.thumbnail
-            } else if (checkbox.checked !== true) {
-              card.style.display = 'block';
-            } else {
-              card.style.display = 'none';
-            }
-          }
-        }
-        return {
-          name: product.name,
-          element: card
-        }
-      })
-
-      // <<<--- Добавление товара в корзину
-      // Добавление товара в корзину --->>>
+      // <<<--- Добавление товара в корзину Добавление товара в корзину --->>>
       let cartProducts = [];
 
       if (localStorage.getItem("RS-cart") === null) {
@@ -76,10 +89,10 @@ function createProducts() {
       cartProducts = JSON.parse(localStorage.getItem("RS-cart"));
 
       for (let i = 0; i < cartProducts.length; i++) {
-        document.getElementById(`btn-add-${cartProducts[i].id}`).innerHTML = 'Drop item';
+        document.getElementById(`btn-add-${cartProducts[i].id}`).innerHTML = 'Drob item';
       }
 
-      for (let i = 0; i < products.length; i++) {
+      for (let i = 0; i < productsRaw.length; i++) {
         document.querySelectorAll('.button__add')[i].addEventListener('click', addToCart);
       }
 
@@ -101,6 +114,27 @@ function createProducts() {
         }
         updateHeader()
       }
+
+      // SORTING
+      sorting.addEventListener("change", (event) => {
+        const [sortBy, sortByDirection] = event.target.value.split('-')
+
+          const sortFn = sortFnByField[sortBy] ?? sortFnByField.default
+          const dir = sortByDirection === 'asc' ? 1 : -1
+      
+          const ary = productsRaw.sort((a, b) => {      
+            return sortFn(a[sortBy], b[sortBy], dir)
+          })
+
+          renderProducts(ary)
+      });
+
+      // search
+      searchInput.addEventListener("input", e => {
+        const value = e.target.value.trim().toLowerCase()
+        const arr = productsRaw.filter(product => product.name.toLowerCase().includes(value))
+        renderProducts(arr)
+      })
     })
 }
 
