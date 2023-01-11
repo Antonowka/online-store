6b0a8bcd9b0b71c8c04724f8d90ff6579d2dc1b9
max7394 <mlysenko92@gmail.com>
2023-01-12 00:54:09 +0300

    fix: filter, sorting and text-search

--- online-store/ts/main.ts
+++ online-store/ts/main.ts
@@ -1,10 +1,12 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable prefer-const */
 // CREATE products item from products.js
-import updateHeader from './updateCart'
-import viewmode from './view-mode'
-import dualInputRange from './input-dual-range'
+import updateHeader from './updateCart';
+import viewmode from './view-mode';
+import dualInputRange from './input-dual-range';
 
-const string = (a: string, b: string, dir: number ): string | number | boolean => a?.localeCompare(b) * dir
-const number = (a: number, b: number, dir: number = 1) => (a - b) * dir
+const string = (a: string, b: string, dir: number): string | number | boolean => a?.localeCompare(b) * dir;
+const number = (a: number, b: number, dir = 1) => (a - b) * dir;
 
 const sortFnByField = {
   default: () => 0,
@@ -13,10 +15,10 @@ const sortFnByField = {
   name: string,
   id: number,
   stock: number,
-}
+};
 
-let productsRaw: Array<HTMLElement> = []
-let filteredProducts: Array<Element> = []
+let productsRaw: Array<HTMLElement> = [];
+let filteredProducts: Array<Element> = [];
 type IProduct = {
   name: string;
   price: number;
@@ -24,28 +26,28 @@ type IProduct = {
   id: number;
   rating: string;
   stock: number;
-}
+};
 
 function createProducts() {
-  const userCardTemplate = document.querySelector("[data-user-template]")
-  const userCardContainer = document.querySelector("[data-user-cards-container]")
-  const searchInput = document.querySelector("[data-search]")
-  const sorting = document.querySelector('.sort-menu')
-  const productContainer = document.querySelector('.products-container')
+  const userCardTemplate = document.querySelector('[data-user-template]');
+  const userCardContainer = document.querySelector('[data-user-cards-container]');
+  const searchInput = document.querySelector('[data-search]');
+  const sorting = document.querySelector('.sort-menu');
+  const productContainer = document.querySelector('.products-container');
 
   function renderProducts(products: Array<IProduct>): void {
     let filteredProducts = products;
-    (productContainer as HTMLElement).innerHTML = ''
+    (productContainer as HTMLElement).innerHTML = '';
 
     filteredProducts.forEach((product: IProduct) => {
       const card = ((userCardTemplate as HTMLTemplateElement).content.cloneNode(true) as HTMLElement).children[0];
-      const info = card.querySelector("[data-name]");
-      const cost = card.querySelector("[data-cost]");
-      const src = card.querySelector("[data-src]");
-      const href = card.querySelector("[data-href]");
-      const btn_add = card.querySelector("[data-btn-add]");
-      const rating = card.querySelector("[data-rating]");
-      const stock = card.querySelector("[data-stock]");
+      const info = card.querySelector('[data-name]');
+      const cost = card.querySelector('[data-cost]');
+      const src = card.querySelector('[data-src]');
+      const href = card.querySelector('[data-href]');
+      const btn_add = card.querySelector('[data-btn-add]');
+      const rating = card.querySelector('[data-rating]');
+      const stock = card.querySelector('[data-stock]');
       (info as HTMLElement).textContent = product.name;
       (cost as HTMLElement).innerHTML = product.price + ' $';
       (src as HTMLImageElement).src = product.thumbnail;
@@ -54,27 +56,26 @@ function createProducts() {
       (stock as HTMLElement).innerHTML = `Stock: ${product.stock}`;
       (userCardContainer as HTMLElement).append(card);
       (btn_add as HTMLElement).id = `btn-add-${product.id}`;
-    })
+    });
   }
 
-  fetch('../js/products.json')
-    .then(res => res.json())
+  fetch('../ts/products.json')
+    .then((res) => res.json())
     .then((data: IProduct) => {
-      (productsRaw as unknown as IProduct) = data
-      renderProducts((productsRaw as unknown as Array<IProduct>))
+      ((productsRaw as unknown) as IProduct) = data;
+      renderProducts((productsRaw as unknown) as Array<IProduct>);
 
       // <<<--- Добавление товара в корзину Добавление товара в корзину --->>>
       // let cartProducts: Array<HTMLElement> = [];
-      localStorage:
-      if (localStorage.getItem("RS-cart") === null) {
+      if (localStorage.getItem('RS-cart') === null) {
         localStorage.setItem('RS-cart', JSON.stringify([]));
       }
 
-      let cartProducts = JSON.parse(localStorage.getItem("RS-cart") || '');
+      let cartProducts = JSON.parse(localStorage.getItem('RS-cart') || '');
 
       for (let i = 0; i < cartProducts.length; i++) {
         (document.getElementById(`btn-add-${cartProducts[i].id}`) as HTMLElement).innerHTML = 'Drob item';
-        (document.getElementById(`btn-add-${cartProducts[i].id}`) as HTMLElement).classList.toggle("drop-active");
+        (document.getElementById(`btn-add-${cartProducts[i].id}`) as HTMLElement).classList.toggle('drop-active');
       }
 
       for (let i = 0; i < productsRaw.length; i++) {
@@ -83,82 +84,87 @@ function createProducts() {
 
       function addToCart(e: Event): void {
         const item_id = parseInt((e.currentTarget as HTMLElement).id.slice(8), 10);
-        if ((document.getElementById(`btn-add-${item_id}`) as HTMLElement).innerHTML === "Add to cart") {
-          (document.getElementById(`btn-add-${item_id}`) as HTMLElement).innerHTML = "Drop item";
-          (document.getElementById(`btn-add-${item_id}`) as HTMLElement).classList.toggle("drop-active");
+        if ((document.getElementById(`btn-add-${item_id}`) as HTMLElement).innerHTML === 'Add to cart') {
+          (document.getElementById(`btn-add-${item_id}`) as HTMLElement).innerHTML = 'Drop item';
+          (document.getElementById(`btn-add-${item_id}`) as HTMLElement).classList.toggle('drop-active');
           cartProducts.push({
-            id: (item_id as unknown as string),
+            id: (item_id as unknown) as string,
             count: 1,
-            price: ((data as unknown as Array<number>)[item_id - 1] as unknown as IProduct).price
-          })
+            price: ((((data as unknown) as Array<number>)[item_id - 1] as unknown) as IProduct).price,
+          });
           localStorage.setItem('RS-cart', JSON.stringify(cartProducts));
         } else {
-          (document.getElementById(`btn-add-${item_id}`) as HTMLElement).innerHTML = "Add to cart";
-          cartProducts = cartProducts.filter((value: { id: unknown | number; }) => value.id != item_id)
+          (document.getElementById(`btn-add-${item_id}`) as HTMLElement).innerHTML = 'Add to cart';
+          cartProducts = cartProducts.filter((value: { id: unknown | number }) => value.id != item_id);
           localStorage.setItem('RS-cart', JSON.stringify(cartProducts));
-          (document.getElementById(`btn-add-${item_id}`) as HTMLElement).classList.remove("drop-active");
+          (document.getElementById(`btn-add-${item_id}`) as HTMLElement).classList.remove('drop-active');
         }
-        updateHeader()
+        updateHeader();
       }
 
       // SORTING
-      (sorting as HTMLSelectElement).addEventListener("change", (event: Event) => {
-        const [sortBy, sortByDirection] = ((event.target as HTMLSelectElement).value).split('-')
+      (sorting as HTMLSelectElement).addEventListener('change', (event: Event) => {
+        let [sortBy, sortByDirection] = (event.target as HTMLSelectElement).value.split('-');
 
-          const sortFn: any = (sortFnByField as unknown as string)[(sortBy as unknown as number)] ?? sortFnByField.default
-          const dir: any = sortByDirection === 'asc' ? 1 : -1
+        let sortFn: any =
+          ((sortFnByField as unknown) as string)[(sortBy as unknown) as number] ?? sortFnByField.default;
+        let dir = sortByDirection === 'asc' ? 1 : -1;
 
-          const ary = filteredProducts.sort((a, b) => {
-            return sortFn((a as unknown as string)[(sortBy as unknown as number)], (b as unknown as string)[(sortBy  as unknown as number)], dir)
-          })
+        let ary = filteredProducts.sort((a, b) => {
+          return sortFn(
+            ((a as unknown) as string)[(sortBy as unknown) as number],
+            ((b as unknown) as string)[(sortBy as unknown) as number],
+            dir
+          );
+        });
 
-          renderProducts((ary as unknown as Array<IProduct>))
+        renderProducts((ary as unknown) as Array<IProduct>);
       });
 
       // search
-      (searchInput as HTMLInputElement).addEventListener("input", e => {
-        const value = ((e.target as HTMLInputElement).value).trim().toLowerCase()
-        if (!value) return renderProducts((productsRaw as unknown as Array<IProduct>))
+      (searchInput as HTMLInputElement).addEventListener('input', (e) => {
+        const value = (e.target as HTMLInputElement).value.trim().toLowerCase();
+        if (!value) return renderProducts((productsRaw as unknown) as Array<IProduct>);
 
-        const ary = productsRaw.filter(product => (product as unknown as IProduct).name.toLowerCase().includes(value))
-        renderProducts((ary as unknown as Array<IProduct>))
-      })
+        const ary = productsRaw.filter((product) =>
+          ((product as unknown) as IProduct).name.toLowerCase().includes(value)
+        );
+        renderProducts((ary as unknown) as Array<IProduct>);
+      });
 
-      type IFilters = {
-        brand?: Array<string>;
-        category?: Array<string>;
-      }
+      type IFilter = {
+        filter: number;
+      };
 
-      type IFilterDom = {
-        filter: string;
-      }
       // filter
-      const checkboxes = document.querySelectorAll('.filter-input')
-      checkboxes.forEach(checkbox => {
+      const checkboxes = document.querySelectorAll('.filter-input');
+      checkboxes.forEach((checkbox) => {
         checkbox.addEventListener('click', () => {
-          const checked = Array.from(checkboxes).filter(checkbox => (checkbox as HTMLInputElement).checked)
+          const checked = Array.from(checkboxes).filter((checkbox) => (checkbox as HTMLInputElement).checked);
 
-          if (!checked.length) return renderProducts((productsRaw as unknown as Array<IProduct>))
+          if (!checked.length) return renderProducts((productsRaw as unknown) as Array<IProduct>);
           const filters = checked.reduce((acc, curr) => {
-              if (!acc[(curr as HTMLInputElement).dataset.filter]) {
-                acc[(curr as HTMLInputElement).dataset.filter] = []
-              }
+            let newArray = acc[((((curr as unknown) as HTMLInputElement).dataset as unknown) as IFilter).filter];
 
-              acc[(curr as HTMLInputElement).dataset.filter].push(curr).value;
+            if (!newArray) {
+              newArray = [];
+            }
 
-              return acc
-          }, {});
+            newArray.push(curr).value;
+            return acc;
+          }, {} as Array<any>);
 
-          const ary: HTMLElement[] = productsRaw.filter(product => {
-            return Object.keys(filters).every(filter => {
-              return (filters as unknown as string)[(filter as unknown as number)].includes((product as unknown as string)[(filter as unknown as number)])
-            })
-          })
-          renderProducts((ary as unknown as Array<IProduct>))
-        })
-      })
-    })
+          const ary: HTMLElement[] = productsRaw.filter((product) => {
+            return Object.keys(filters).every((filter) => {
+              return ((filters as unknown) as string)[(filter as unknown) as number].includes(
+                ((product as unknown) as string)[(filter as unknown) as number]
+              );
+            });
+          });
+          renderProducts((ary as unknown) as Array<IProduct>);
+        });
+      });
+    });
 }
 
-
-export {createProducts, viewmode, dualInputRange}
+export { createProducts, viewmode, dualInputRange };
